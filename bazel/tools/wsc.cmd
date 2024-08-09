b434b697c41bb006adba2a0b9ddc2ce33eb0cb4f
Ezekiel Warren <ezekiel@seaube.com>
2024-08-09 14:12:22 -0700

    fix: no stamping because it doesn't work

--- bazel/tools/wsc.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-for /f %%i in ('git describe --tags --abbrev^=0') do (
-	echo STABLE_GIT_RELEASE_TAG %%i
-)
-
--- bazel/tools/wsc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-if [[ -z "${GITHUB_REF}" ]]; then
-	echo "STABLE_GIT_RELEASE_TAG $(git describe --tags --abbrev=0)"
-else
-	echo "STABLE_GIT_RELEASE_TAG ${GITHUB_REF##*/}"
-fi
